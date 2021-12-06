f83b3ad8bdeaa75f8386fc043a66244a458e378d
LiraViictor <victor.liracarlos@gmail.com>
2021-12-05 22:59:34 -0300

    criado function para abrir e fechar modal

--- assets/script.js
+++ assets/script.js
@@ -8,3 +8,18 @@ setInterval(() => {
         numb.textContent = counter + "%";
     }
 }, 80)
+
+function iniciarModal(modalID) {
+    const modal = document.getElementById(modalID);
+    if(modal) {
+        modal.classList.add('mostrar');
+        modal.addEventListener('click', (e) => {
+            if(e.target.id === modalID || e.target.className === 'fechar') {
+                modal.classList.remove('mostrar');
+            }
+        })
+    }
+}
+
+const addButton = document.querySelector('.card.card-add');
+addButton.addEventListener('click', () => iniciarModal('modal-tarefa'));
