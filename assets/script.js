a1e87829e3d54389a13b0ecd6609b924ec5ca94b
LiraViictor <victor.liracarlos@gmail.com>
2021-12-06 20:08:10 -0300

    adicionado timer (WIP), com start, stop and reset

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,52 @@
-const numb = document.querySelector(".numb");
-let counter = 0;
-setInterval(() => {
-    if(counter == 100){
+/* TIMER -------------------*/
+var tempo = 1000;
+var cronos;
+var startingMinutes = 25;
+var counter;
+var root;
+
+function start() {
+    timer();
+}
+
+function pause() {
+    clearInterval(cronos);
+}
+
+function timer(root) {
+
+    //startingMinutes = document.getElementById("minuto").value;
+    let time = startingMinutes * 60;
+
+    counter = document.getElementById("counter");
+
+    const numb = document.querySelector(".numb");
+    cronos = setInterval(() => {updateCountdown();
+    if(counter == 0){
         clearInterval();
-    } else {
-        counter+=1;
-        numb.textContent = counter + "%";
+    }}, tempo)
+
+    function updateCountdown() {
+        let minutes = Math.floor(time / 60);
+        let seconds = time % 60;
+
+        minutes = minutes < 10 ? '0' + minutes : minutes;
+        seconds = seconds < 10 ? '0' + seconds : seconds;
+
+        counter.innerHTML = `${minutes}:${seconds}`;
+        time--;
     }
-}, 80)
+}
+
+function reset() {
+    clearInterval(cronos)
+    startingMinutes = 0;
+    counter.innerHTML = `00:00`
+}
+
+    
+/*  MODAL -----------------------*/
+    
 
 function iniciarModal(modalID) {
     const modal = document.getElementById(modalID);
@@ -23,3 +62,6 @@ function iniciarModal(modalID) {
 
 const addButton = document.querySelector('.card.card-add');
 addButton.addEventListener('click', () => iniciarModal('modal-tarefa'));
+
+
+/* NOVA TAREFA -----------------------*/
